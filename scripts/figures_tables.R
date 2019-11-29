7e1a4a1adfef9cfed1d6429af934457bf4fc6dbb
James Santangelo <james.santangelo37@gmail.com>
2019-11-28 20:21:24 -0500

    Moved figures to separate script. Documented figure 2 and added figure 3a

--- scripts/figures_tables.R
+++ scripts/figures_tables.R
@@ -13,6 +13,7 @@ library(tidyverse)
 ################
 #### TABLES ####
 ################
+
 ## TABLE 1 
 
 # Linear cline models only. Part of table 1
@@ -93,4 +94,271 @@ tableS4_HCN_dredge_models <- read_csv("analysis/tables/supplemental/TableS4_HCN_
 # It can be loaded below
 tableS5_fullCoeffs <- read_csv("analysis/tables/supplemental/TableS5_freqHCN_FullModelAvg.csv")
 
+#### FIGURES ####
 
+#Theme used to plot figures throughout script.
+ng1 = theme(
+  aspect.ratio = 0.7,
+  panel.background = element_blank(),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  panel.border = element_blank(),
+  axis.line.x = element_line(color = "black", size = 1),
+  axis.line.y = element_line(color = "black", size = 1),
+  axis.ticks = element_line(color = "black"),
+  axis.text = element_text(color = "black", size = 15),
+  axis.title = element_text(color = "black", size = 1),
+  axis.title.y = element_text(vjust = 2, face = "bold", size = 18),
+  axis.title.x = element_text(vjust = 0.1, face = "bold", size = 18),
+  axis.text.x = element_text(size = 16),
+  axis.text.y = element_text(size = 16),
+  legend.position = "none",
+  legend.direction = "vertical",
+  legend.text = element_text(size = 13),
+  legend.key = element_rect(fill = "white"),
+  legend.title = element_text(size = 15, face = "bold"),
+  legend.key.size = unit(1.0, "cm")
+)
+
+## FIGURE 1
+
+# Figure 1 was generated in QGIS (v. 3.2.3) and is not included here
+
+## FIGURE 2 
+
+# Plot of HCN frequency against distance for each city
+# Solid line if significant. Thick black line is regression across all cities.
+HCN_by_city <- datPops %>%
+  group_by(City) %>%
+  do(mod = lm(freqHCN ~ std_distance, data = .)) %>% # Linear model for each city
+  tidy(., mod) %>% # Clean up coefficients
+  mutate(intercept = estimate[term == "(Intercept)"]) %>%
+  filter(term == "std_distance") %>% # Only need beta coefficient
+  mutate(predicted = intercept + (estimate * 1.0)) %>% # Calculate predicted value along regression line for each city
+  select(City, p.value, predicted) %>%
+  merge(., datPops, by = "City", all.y = TRUE) %>% # Merge predicted values for each city to pop-mean dataframe
+  mutate(significant = ifelse(p.value < 0.05, "Yes", "No")) %>%
+  ggplot(., aes(x = std_distance, y = freqHCN)) +  
+  # Regression line for each city
+  geom_line(stat = "smooth", method="lm", 
+            aes(linetype = significant,
+                group = City), 
+            alpha = 0.7, size = 1) +
+  # Overall regression line
+  geom_line(stat = "smooth", method="lm", colour = "black", size = 2.5) +
+  # scale_colour_manual(values = colors) +
+  xlab("Standardized distance") + ylab("Frequency of HCN") + 
+  scale_linetype_manual(values=c("dashed", "solid")) +
+  scale_y_continuous(breaks = seq(from = 0.1, to = 1, by = 0.1)) +
+  scale_x_continuous(breaks = seq(from = 0, to = 1.1, by = 0.25)) +
+  coord_cartesian(ylim = c(0.1, 1.025), xlim = c(0, 1), clip = 'off') +
+  geom_text(aes(label = City, x = 1.005, y = predicted), hjust = 0) + 
+  ng1 +
+  theme(legend.position = "top", legend.direction = "horizontal",
+        legend.key.height = unit(0.5, "cm")) +
+  guides(color = guide_legend(override.aes = list(size = 2)))
+# geom_dl(aes(label = City), method = list(dl.trans(x = x + 0.2), "last.points", cex = 0.8))
+HCN_by_city
+
+# Save file to disk
+ggsave(filename = "analysis/figures/main-text/from_R/Figure2_HCN-by-distance.pdf", 
+       plot = HCN_by_city, device = 'pdf', units = 'in',
+       width = 12, height = 8, dpi = 600)
+
+
+
+## FIGURE 3 ##
+
+citySummaryData <- read_csv("data-clean/citySummaryData.csv")
+  
+# Add abbreviations
+citySummaryData <- citySummaryData %>%
+  mutate(abbr = case_when(City == "Jacksonville" ~ "Jax",
+                          City == "Tampa" ~ "Tpa",
+                          City == "Atlanta" ~ "Atl",
+                          City == "Norfolk" ~ "Nor",
+                          City == "Charlotte" ~ "Clt",
+                          City == "Toronto" ~ "Tor",
+                          City == "Montreal" ~ "Mtl",
+                          City == "Detroit" ~ "Det",
+                          City == "Washington D.C." ~ "DC",
+                          City == "Cleveland" ~ "Clv",
+                          City == "NewYork" ~ "NY",
+                          City == "Pittsburgh" ~ "Pgh",
+                          City == "Boston" ~ "Bos",
+                          City == "Baltimore" ~ "Blt",
+                          City == "Cincinnati" ~ "Cin",
+                          TRUE ~ "Phl"))
+
+
+# Figure 3a. HCN against # days < 0 with no snow
+HCN_by_DaysNeg <- citySummaryData %>%
+  ggplot(., aes(x = daysNegNoSnow, y = freqHCN)) +
+  # geom_point(size = 2.5) +
+  geom_smooth(method = "lm", size = 1.5, colour = "black", 
+              se = FALSE) +
+  scale_x_continuous(breaks = seq(from = 0, to = 35, by = 5)) +
+  xlab("# days < 0°C with no snow") + ylab("Mean HCN frequency") +
+  geom_text(aes(label = abbr), vjust = 0, hjust = 0) + 
+  ng1
+HCN_by_DaysNeg
+
+ggsave(filename = "analysis/figures/main-text/from_R/Figure3a_HCN-by-NumDaysNegNoSnow.pdf", 
+       plot = HCN_by_DaysNeg, device = 'pdf', units = 'in',
+       width = 5, height = 5, dpi = 600)
+
+# Figure 3b. HCN against PC1
+HCN_by_PC1 <- citySummaryData %>%
+  ggplot(., aes(x = PC1_HCN, y = freqHCN)) +
+  # geom_point(size = 2.5) +
+  geom_smooth(method = "lm", size = 1.5, colour = "black", 
+              se = FALSE) +
+  # scale_x_continuous(breaks = seq(from = 0, to = 35, by = 5)) +
+  xlab("PC1 (90.2%)") + ylab("Mean HCN frequency") +
+  geom_text(aes(label = abbr), vjust = 0, hjust = 0) + 
+  ng1
+HCN_by_PC1
+
+ggsave(filename = "analysis/figures/main-text/from_R/Figure3b_HCN-by-PC1.pdf", 
+       plot = HCN_by_PC1, device = 'pdf', units = 'in',
+       width = 5, height = 5, dpi = 600)
+
+
+## FIGURE 4 
+
+citySummaryDataForAnalysis <- citySummaryDataForAnalysis %>% 
+  left_join(., citySummaryData %>% select(City, abbr), by = "City")
+
+# Figure 4. Slope against PC1
+Slope_by_PC1 <- citySummaryDataForAnalysis %>%
+  ggplot(., aes(x = PC1_Slope, y = cyanSlopeForAnalysis)) +
+  # geom_point(size = 2.5) +
+  geom_smooth(method = "lm", size = 1.5, colour = "black", 
+              se = FALSE) +
+  # scale_x_continuous(breaks = seq(from = 0, to = 35, by = 5)) +
+  xlab("PC1 (92.8%)") + ylab("Slope of HCN cline") +
+  geom_text(aes(label = abbr), vjust = 0, hjust = 0) + 
+  ng1
+Slope_by_PC1
+
+ggsave(filename = "analysis/figures/main-text/from_R/Figure4_Slope-by-PC1.pdf", 
+       plot = Slope_by_PC1, device = 'pdf', units = 'in',
+       width = 5, height = 5, dpi = 600)
+
+## FIGURE S1
+
+# Figure S1 is generated with scripts/powerAnalysis.R
+
+## FIGURE S2
+
+#Plot of HCN frequencies with latitude
+plotHCN_by_lat <- ggplot(citySummaryData, aes(x = Latitude, y = freqHCN)) +
+  geom_point(colour = "black", size = 3.5) +
+  geom_smooth(method = "lm", se = FALSE, colour = "black", size = 2) + 
+  ylab("Frequency of HCN") + xlab("Latitude") +
+  ng1
+plotHCN_by_lat
+
+ggsave(filename = "analysis/figures/supplemental/from_R/FigureS2_HCN_by_Lat.pdf", 
+       plot = plotHCN_by_lat, device = "pdf", 
+       width = 5, height = 5, dpi = 300)
+
+## FIGURE S3
+
+Ac_Haplo_Freqs <- ggplot(freqHaploAc, aes(x = Habitat, y = freqHaploAc, fill = haplotype_Ac)) +
+  geom_bar(stat='identity') + 
+  xlab("Habitat") + ylab("Haplotype frequency") + 
+  facet_grid(~City) +
+  scale_fill_manual(values = c("#F98400", "#5BBCD6")) +
+  ng1 + theme(legend.position = "top",
+              legend.direction = "horizontal",
+              aspect.ratio=3.0, legend.text=element_text(size=10),
+              legend.title = element_text(size = 0), 
+              legend.key.size = unit(0.5, "cm"),
+              axis.text.x = element_text(angle = 45, hjust = 1))
+Ac_Haplo_Freqs
+
+ggsave(filename = 'analysis/figures/supplemental/from_R/FigureS3_Ac_haplotype_freqs.pdf', 
+       plot = Ac_Haplo_Freqs, device = "pdf", 
+       width = 8, height = 5, dpi = 300)
+
+## FIGURE S4
+
+Li_Haplo_Freqs <- ggplot(freqHaploLi, aes(x = Habitat, y = freqHaploLi, fill = haplotype_Li)) +
+  geom_bar(stat='identity') + 
+  xlab("Habitat") + ylab("Haplotype frequency") + 
+  facet_grid(~City) +
+  scale_fill_manual(values = c("#00A08A", "#F2AD00", "#F98400", "#5BBCD6")) +
+  ng1 + theme(legend.position = "top",
+              legend.direction = "horizontal",
+              aspect.ratio=3.0, legend.text=element_text(size=10),
+              legend.title = element_text(size = 0), 
+              legend.key.size = unit(0.5, "cm"),
+              axis.text.x = element_text(angle = 45, hjust = 1))
+Li_Haplo_Freqs
+
+ggsave(filename = 'analysis/figures/supplemental/from_R/FigureS4_Li_haplotype_freqs.pdf', 
+       plot = Li_Haplo_Freqs, device = "pdf", 
+       width = 8, height = 5, dpi = 300)
+
+## FIGURES S5 - S20 (INDIVIDUAL CLINE BIPLOTS)
+
+# Create list with city dataframes as elements
+city_df_list <- datPops %>% split(.$City)
+
+# Create biplot for all cities with HCN as response
+outpath <- "analysis/figures/individualCline_biplots/HCN/"
+purrr::walk(city_df_list, clineBiplot, 
+            response_var = "freqHCN", 
+            outpath = outpath,
+            model_order_df = clineModelOrder)
+
+# Create biplot for all cities with Ac as response
+outpath <- "analysis/figures/individualCline_biplots/Ac/"
+purrr::walk(city_df_list, clineBiplot, 
+            response_var = "AcHWE", 
+            outpath = outpath,
+            model_order_df = clineModelOrder)
+
+# Create biplot for all cities with Li as response
+outpath <- "analysis/figures/individualCline_biplots/Li/"
+purrr::walk(city_df_list, clineBiplot, 
+            response_var = "LiHWE", 
+            outpath = outpath,
+            model_order_df = clineModelOrder)
+
+## FIGURE SXX ##
+
+# HCN by city logistic
+HCN_by_cityLog <- datPlants %>%
+  group_by(City) %>%
+  do(mod = glm(HCN_Result ~ std_distance, family = "binomial", data = .)) %>%
+  tidy(., mod) %>%
+  mutate(intercept = estimate[term == "(Intercept)"]) %>%
+  filter(term == "std_distance") %>%
+  mutate(predicted = intercept + (estimate * 1.0),
+         odds = exp(predicted),
+         prob = odds / (1 + odds)) %>%
+  select(City, p.value, predicted, odds, prob) %>%
+  merge(., datPlants, by = "City", all.y = TRUE) %>%
+  mutate(significant = ifelse(p.value < 0.05, "Yes", "No")) %>%
+  ggplot(., aes(x = std_distance, y = HCN_Result)) +  
+  geom_line(stat = "smooth", method="glm", method.args = list(family = "binomial"),
+            aes(linetype = significant,
+                group = City), 
+            alpha = 0.7, size = 1) +
+  geom_line(stat = "smooth", method="glm", method.args = list(family = "binomial"),
+            colour = "black", size = 2.5) +
+  # scale_colour_manual(values = colors) +
+  xlab("Standardized distance") + ylab("Probability of being HCN+") + 
+  scale_linetype_manual(values=c("dashed", "solid")) +
+  scale_y_continuous(breaks = seq(from = 0, to = 1, by = 0.25)) +
+  scale_x_continuous(breaks = seq(from = 0, to = 1.1, by = 0.25)) +
+  coord_cartesian(ylim = c(0, 1.025), xlim = c(0, 1), clip = 'off') +
+  geom_text(aes(label = City, x = 1.005, y = prob), hjust = 0) + 
+  ng1 +
+  theme(legend.position = "top", legend.direction = "horizontal",
+        legend.key.height = unit(0.5, "cm")) +
+  guides(color = guide_legend(override.aes = list(size = 2)))
+# geom_dl(aes(label = City), method = list(dl.trans(x = x + 0.2), "last.points", cex = 0.8))
+HCN_by_cityLog
